baac78645733cc95895fb9f3d5f7e205e2249d85
Tomek Mularczyk <tomaszmularczyk89@gmail.com>
2020-03-08 12:17:56 +0100

    feat: add TabPage component

---
+++ src/TabPage.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+
+interface Props {
+  children?: React.ReactNode;
+  isOpen: boolean;
+  innerRef?: React.MutableRefObject<HTMLDivElement | null>;
+}
+
+const TRANSITION_DURATION = 0.3;
+
+function useBodyScrollLock(
+  isPageOpen: boolean,
+  pageRef: React.RefObject<HTMLDivElement>
+) {
+  React.useEffect(() => {
+    if (isPageOpen) {
+      disableBodyScroll(pageRef.current);
+    }
+
+    const node = pageRef.current;
+    return () => {
+      enableBodyScroll(node);
+    };
+  }, [isPageOpen, pageRef]);
+}
+
+function useOuterContainer() {
+  const ref = React.useRef<HTMLDivElement>();
+  React.useEffect(() => {
+    ref.current = document.createElement("div");
+    document.body.appendChild(ref.current);
+
+    const node = ref.current;
+    return () => {
+      document.body.removeChild(node);
+    };
+  }, []);
+
+  return ref.current;
+}
+
+function usePageTransition(isOpen: boolean) {
+  React.useEffect(() => {
+    const mainContainer = document.getElementById("root");
+    if (mainContainer) {
+      mainContainer.style.transition = `all ${TRANSITION_DURATION}s ease-out`;
+    }
+    return () => {
+      if (mainContainer) {
+        mainContainer.style.removeProperty("transition");
+      }
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const mainContainer = document.getElementById("root");
+    if (isOpen && mainContainer) {
+      mainContainer.style.transform = "translate(-100vw)";
+    }
+
+    return () => {
+      if (mainContainer) mainContainer.style.removeProperty("transform");
+    };
+  }, [isOpen]);
+}
+
+export const TabPage = ({ children, isOpen, innerRef }: Props) => {
+  const pageRef = React.useRef<HTMLDivElement | null>(null);
+  const container = useOuterContainer();
+  useBodyScrollLock(isOpen, pageRef);
+  usePageTransition(isOpen);
+
+  if (container) {
+    return ReactDOM.createPortal(
+      <div
+        style={{
+          position: "fixed",
+          overflowY: "scroll",
+          backgroundColor: "white",
+          width: "100vw",
+          top: 0,
+          height: "100vh",
+          transition: `left ${TRANSITION_DURATION}s ease-out`,
+          left: isOpen ? 0 : "100vw",
+          /* Fixes iOS vertical scroling through containers with horizontal scroll
+             https://stackoverflow.com/questions/41681251/overflow-x-scroll-inside-a-fixed-div-prevents-vertical-scrolling-on-ios */
+          WebkitOverflowScrolling: "touch"
+        }}
+        ref={el => {
+          pageRef.current = el;
+          if (innerRef) {
+            // eslint-disable-next-line no-param-reassign
+            innerRef.current = el;
+          }
+        }}
+      >
+        {children}
+      </div>,
+      container
+    );
+  }
+
+  return null;
+};
